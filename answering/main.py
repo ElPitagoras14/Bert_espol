ca6e74fe553869f61c5eff405dd37dc524d449e3
Jonathan Garcia <jonfrgar@espol.edu.ec>
2021-09-22 16:32:07 -0500

    Add preguntas frecuentes

--- answering/main.py
+++ answering/main.py
@@ -1,10 +1,15 @@
 import transformers
 import matplotlib.pyplot as plt
 from transformers import pipeline
-from .contexto import *
 
 question_answering = pipeline("question-answering", model="mrm8488/bert-multi-cased-finetuned-xquadv1", tokenizer="mrm8488/bert-multi-cased-finetuned-xquadv1")
 
+def context(archivo):
+    contxt = []
+    file = open("resources/" + archivo, 'r', encoding="utf-8")
+    contxt = file.readlines()[1]
+    file.close()
+    return contxt
 
 def respuesta(categoria, pregunta = "¿Cuándo se fundó espol?"):
   ctx = context("ctx_" + categoria + ".txt")
@@ -12,8 +17,8 @@ def respuesta(categoria, pregunta = "¿Cuándo se fundó espol?"):
   archivo = open("resources/historial.txt", "a", encoding="utf-8")
   answer = result["answer"]
   score = result["score"]
-  archivo.write("R:" + answer.capitalize() + "\n")
-  archivo.write("P:" + pregunta.capitalize() + "\n")
+  archivo.write("R: " + answer.capitalize() + "\n")
+  archivo.write("P: " + pregunta.capitalize() + "\n")
   archivo.close()
   return (answer, score)
 
@@ -22,7 +27,11 @@ def cargarHistorial():
   lineas = archivo.readlines()
   reverso = lineas[-8:]
   reverso.reverse()
+  archivo.close()
   return reverso
 
 def frecuentes(categoria):
-  return ""
+  archivo = open("resources/frec_" + categoria + ".txt", "r", encoding="utf-8")
+  res = archivo.readlines()
+  archivo.close()
+  return res
